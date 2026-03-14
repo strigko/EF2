a6ecbaae522dcbdabb6f805c19e30b3f07623cb5
strigko <planmobi844@gmail.com>
2026-03-14 19:27:21 +0200

    Lesson 5: EF

---
+++ DapperCase/Entity/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFP48.DapperCase.Entity
+{
+    internal class User
+    {
+        public Guid Id { get; set; }
+        public string Name{ get; set; }
+        public string Surname{ get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $@"
+-------------------------------------
+UserID: {Id}
+Name -> {Name}
+Surname -> {Surname}
+Age -> {Age}
+-------------------------------------
+";
+        }
+    }
+}
---
+++ DapperCase/Program.cs
@@ -0,0 +1,224 @@
+﻿using Azure;
+using Dapper;
+using EFP48.DapperCase.Entity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFP48
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\EFP48\db_dapper.mdf;Integrated Security=True";
+
+            using IDbConnection connection = new SqlConnection(connectionString);
+
+            string query = @"
+CREATE TABLE Users
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY,
+    Name NVARCHAR(255) NOT NULL,
+    Surname NVARCHAR(255) NOT NULL,
+    Age INT NOT NULL
+
+);
+";
+            //connection.Execute(query);
+
+
+            var users = new List<User>
+            {
+                new User{ Id = Guid.NewGuid(), Name = "Tom", Surname = "Due", Age = 25},
+                new User{ Id = Guid.NewGuid(), Name = "Alex", Surname = "Jackson", Age = 25},
+                new User{ Id = Guid.NewGuid(), Name = "Bill", Surname = "White", Age = 25},
+            };
+
+            query = @"INSERT INTO Users(Id, Name, Surname, Age) VALUES(@Id,@Name,@Surname,@Age)";
+/*            connection.Execute(query, users);*/
+
+
+            query = @"SELECT * FROM Users as u ORDER BY u.Age DESC";
+
+            var q_users = connection.Query<User>(query).ToList();
+
+            if(q_users != null)
+            {
+                foreach(var item in q_users)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Guid searchId = Guid.Parse("91437934-bc67-4906-980b-1243d1642849");
+            query = @"SELECT * FROM Users as u WHERE u.Id = @Id";
+            var user = connection.QueryFirstOrDefault<UserCard>(query, new { Id = searchId });
+
+            if(user != null) Console.WriteLine(user);
+
+            query = @"
+DROP TABLE IF EXISTS Posts;
+CREATE TABLE Posts
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    UserId UNIQUEIDENTIFIER NOT NULL,
+    Title NVARCHAR(200),
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+    DeletedAt DATETIME DEFAULT NULL,
+    CONSTRAINT fk_post_user FOREIGN KEY (UserId) REFERENCES Users(Id)
+);
+";
+/*            connection.Execute(query);*/
+
+            query = @"INSERT INTO Posts(UserId, Title) VALUES(@UserId, @Title)";
+
+            var list = new[]
+            {
+                new { UserId = Guid.Parse("7c35246d-a06d-480b-8a8c-531f742b8b16"), Title = "That`s a fine day!" },
+                new { UserId = Guid.Parse("c4767365-09b7-4b5a-87a1-a3bef27f7e30"), Title = "Hello world"} 
+            }.ToList();
+
+            /*            connection.Execute(query, list);*/
+
+            query = @"
+            CREATE TABLE Comments
+            (
+                Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+                PostId UNIQUEIDENTIFIER NOT NULL,
+                UserId UNIQUEIDENTIFIER NOT NULL,
+                Content NVARCHAR(500),
+                CreatedAt DATETIME DEFAULT GETDATE(),
+
+                CONSTRAINT fk_comment_post FOREIGN KEY(PostId) REFERENCES Posts(Id),
+                CONSTRAINT fk_comment_user FOREIGN KEY(UserId) REFERENCES Users(Id)
+            );
+
+            CREATE TABLE PostLikes
+            (
+                Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+                PostId UNIQUEIDENTIFIER NOT NULL,
+                UserId UNIQUEIDENTIFIER NOT NULL,
+
+                CONSTRAINT fk_post_like FOREIGN KEY(PostId) REFERENCES Posts(Id),
+                CONSTRAINT fk_post_like_user FOREIGN KEY(UserId) REFERENCES Users(Id)
+            );
+
+            CREATE TABLE CommentLikes
+            (
+                Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+                CommentId UNIQUEIDENTIFIER NOT NULL,
+                UserId UNIQUEIDENTIFIER NOT NULL,
+
+                CONSTRAINT fk_comment_like FOREIGN KEY(CommentId) REFERENCES Comments(Id),
+                CONSTRAINT fk_comment_like_user FOREIGN KEY(UserId) REFERENCES Users(Id)
+            );";
+            /*            connection.Execute(query);*/
+
+            query = @"INSERT INTO Comments(PostId, UserId, Content) 
+VALUES(@PostId, @UserId, @Content)";
+
+            var comments = new[]
+            {
+    new {
+        PostId = Guid.Parse("228cc7a5-0015-4513-8252-1c15e1c50860"),
+        UserId = Guid.Parse("7c35246d-a06d-480b-8a8c-531f742b8b16"),
+        Content = "Nice post!"
+    },
+    new {
+        PostId = Guid.Parse("228cc7a5-0015-4513-8252-1c15e1c50860"),
+        UserId = Guid.Parse("03a90025-ff3a-4812-9abb-ec3fa04ca510"),
+        Content = "I agree!"
+    }
+};
+
+            connection.Execute(query, comments);
+
+            query = @"INSERT INTO CommentLikes(CommentId, UserId)
+VALUES(@CommentId, @UserId)";
+
+            var likes = new[]
+            {
+    new {
+        CommentId = Guid.Parse("ff5b8253-47cf-451d-bc29-840b1f4ad02f"),
+        UserId = Guid.Parse("c4767365-09b7-4b5a-87a1-a3bef27f7e30")
+    },
+    new {
+        CommentId = Guid.Parse("ff5b8253-47cf-451d-bc29-840b1f4ad02f"),
+        UserId = Guid.Parse("6e20ee03-d0e1-4734-b142-ff4ba3038883")
+    }
+};
+
+            connection.Execute(query, likes);
+
+
+            query = @"
+SELECT p.Title, c.Content, COUNT(cl.Id) AS Likes
+FROM Posts p
+JOIN Comments c ON c.PostId = p.Id
+LEFT JOIN CommentLikes cl ON cl.CommentId = c.Id
+GROUP BY p.Title, c.Content
+ORDER BY Likes DESC";
+
+var result = connection.Query(query);
+
+foreach(var item in result)
+{
+    Console.WriteLine($"{item.Title} | {item.Content} | Likes: {item.Likes}");
+}
+
+            query = @"
+SELECT p.Title, COUNT(c.Id) AS CommentCount
+FROM Posts p
+LEFT JOIN Comments c ON c.PostId = p.Id
+GROUP BY p.Title";
+
+            var commentsCount = connection.Query(query);
+
+            foreach (var item in commentsCount)
+            {
+                Console.WriteLine($"{item.Title} -> {item.CommentCount} comments");
+            }
+
+
+            query = @"
+SELECT TOP 1 u.Name, u.Surname, COUNT(cl.Id) AS TotalLikes
+FROM Users u
+JOIN Comments c ON c.UserId = u.Id
+JOIN CommentLikes cl ON cl.CommentId = c.Id
+GROUP BY u.Name, u.Surname
+ORDER BY TotalLikes DESC";
+
+            var topUser = connection.QueryFirstOrDefault(query);
+
+            Console.WriteLine($"Top user: {topUser.Name} {topUser.Surname} | Likes: {topUser.TotalLikes}");
+
+
+
+        }
+    }
+    class UserCard
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public override string ToString()
+        {
+            return $@"
+-------------------------------------
+Name -> {Name}
+Surname -> {Surname}
+-------------------------------------
+";
+        }
+    }
+}
